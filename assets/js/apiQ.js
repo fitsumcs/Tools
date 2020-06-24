4a798e949bd0f715b2ce29211edca6f1117cb19e
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-25 04:55:50 +0530

    Password api fetch class initiated

---
+++ assets/js/apiQ.js
@@ -0,0 +1,21 @@
+class API
+{
+  constructor()
+  {
+  
+  }
+
+
+//   
+async getPassword()
+{
+  
+   const response = await fetch();
+   const data = await response.json();
+
+   return data;
+
+
+}
+
+}
