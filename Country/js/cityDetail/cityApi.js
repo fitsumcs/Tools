9a5c143f71fceebece82484bef7d438776681959
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-28 11:17:16 +0300

    No-cors enabled

--- Country/js/cityDetail/cityApi.js
+++ Country/js/cityDetail/cityApi.js
@@ -10,6 +10,7 @@ async getCountriesList()
    
   
    const response = await fetch("https://countries-cities.p.rapidapi.com/location/country/list?format=json", {
+      "mode":"no-cors",   
       "method": "GET",
       "headers": {
          "x-rapidapi-host": "countries-cities.p.rapidapi.com",
@@ -28,9 +29,10 @@ async getCountryDetail(Id)
    
   
    const response = await fetch(`https://countries-cities.p.rapidapi.com/location/country/${Id}?format=json`, {
-      "method": "GET",
+     "mode":"no-cors",
+     "method": "GET",
       "headers": {
-         "x-rapidapi-host": "countries-cities.p.rapidapi.com",
+         "x-rapidapi-host": "countrises-cities.p.rapidapi.com",
          "x-rapidapi-key": "64bca0886bmsha7377930000aed5p1a6aecjsn8fa7b68dbbfb"
       }
    })
